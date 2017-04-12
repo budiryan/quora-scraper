46190c1b34830cd2a536a9b73add8a0949ba38c5
budi <budiryan@github.com>
2017-04-12 10:12:50 +0800

    zzzz

--- scrape.py
+++ scrape.py
@@ -20,6 +20,12 @@ BASE_URL = 'https://www.quora.com'
 # File path
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 FILE_DIRECTORY = os.path.join(PROJECT_ROOT, 'user_links.json')
+USER_OUTPUT_FILE = os.path.join(PROJECT_ROOT, 'users.json')
+ANSWER_OUTPUT_FILE = os.path.join(PROJECT_ROOT, 'answers.json')
+
+
+users_result = []
+answers_result = []
 
 
 def login(driver):
@@ -27,7 +33,7 @@ def login(driver):
     # Find sign-in by Google button and click it
     elem = driver.find_element_by_class_name("google_button")
     elem.click()
-    time.sleep(8)
+    time.sleep(5)
     window_before = driver.window_handles[0]
     window_after = driver.window_handles[1]
 
@@ -61,6 +67,126 @@ def login(driver):
     driver.switch_to_window(window_before)
 
 
+def process_user(driver, writer_url):
+    url = urllib.parse.urljoin(BASE_URL, writer_url)
+    driver.get(url)
+    # have to scroll until the end of page
+    current_html = driver.find_element_by_class_name('ContentWrapper')
+    current_html = current_html.get_attribute('innerHTML')
+    stuck_value = 0
+
+    while(True):
+        prev_html = current_html
+        # scroll to the end of page and set some delay --> to get the questions
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        current_html = driver.find_element_by_class_name('ContentWrapper')
+        current_html = current_html.get_attribute("innerHTML")
+        time.sleep(5)
+
+        if stuck_value > 3:
+            break
+
+        if prev_html == current_html:
+            # if after scrolling nothing changes, that means it is stuck
+            stuck_value += 1
+
+        print('stuck_value: ', stuck_value)
+
+    # have to get the info of each user
+    name_and_signature = driver.find_element_by_class_name('ProfileNameAndSig')
+    name = name_and_signature.find_element_by_class_name('user').text
+    try:
+        description = name_and_signature.find_element_by_class_name('UserCredential').text
+    except:
+        description = ''
+    credentials_and_highlights = driver.find_element_by_class_name('AboutSection')
+    try:
+        education = credentials_and_highlights.find_element_by_class_name('SchoolCredentialListItem')
+        education = re.sub(r'Studied at', '', education.find_element_by_class_name('UserCredential').text).strip()
+    except:
+        education = ''
+    try:
+        lives_in = credentials_and_highlights.find_element_by_class_name('LocationCredentialListItem')
+        lives_in = re.sub(r'Lives in', '', lives_in.find_element_by_class_name('UserCredential').text).strip()
+    except:
+        lives_in = ''
+    try:
+        work = credentials_and_highlights.find_element_by_class_name('WorkCredentialListItem')
+        work = work.find_element_by_class_name('UserCredential')
+    except:
+        work = ''
+
+    num_answers = int(re.sub(',', '', driver.find_element_by_class_name('AnswersNavItem').find_element_by_class_name('list_count').text))
+    num_questions = int(re.sub(',', '', driver.find_element_by_class_name('QuestionsNavItem').find_element_by_class_name('list_count').text))
+    num_posts = int(re.sub(',', '', driver.find_element_by_class_name('PostsNavItem').find_element_by_class_name('list_count').text))
+    num_blogs = int(re.sub(',', '', driver.find_element_by_class_name('BlogsNavItem').find_element_by_class_name('list_count').text))
+    num_followers = int(re.sub(',', '', driver.find_element_by_class_name('FollowersNavItem').find_element_by_class_name('list_count').text))
+    num_following = int(re.sub(',', '', driver.find_element_by_class_name('FollowingNavItem').find_element_by_class_name('list_count').text))
+    num_topics = int(re.sub(',', '', driver.find_element_by_class_name('TopicsNavItem').find_element_by_class_name('list_count').text))
+
+    users_result.append({
+        'author_name': name,
+        'auhtor_url': url,
+        'description': description,
+        'education': education,
+        'lives_in': lives_in,
+        'num_answers': num_answers,
+        'num_questions': num_questions,
+        'num_posts': num_posts,
+        'num_blogs': num_blogs,
+        'num_followers': num_followers,
+        'num_following': num_following,
+        'num_topics': num_topics
+    })
+    with open(USER_OUTPUT_FILE, 'w') as f:
+        json.dump(users_result, f, indent=4)
+
+    answers = driver.find_element_by_class_name('layout_3col_center')
+
+    answer_links = answers.find_elements_by_class_name('answer_text')
+    # no answer at all, no need to collect anything
+    if len(answer_links) == 0:
+        return
+    driver.execute_script("window.scrollTo(0, 0);")
+    time.sleep(2)
+    for answer_link in answer_links:
+        driver.execute_script("window.scrollTo(0, 0);")
+        time.sleep(5)
+        answer_link.find_element_by_class_name('more_link').click()
+        time.sleep(5)
+        the_modal = driver.find_element_by_xpath("//*[@class='modal_overlay feed_desktop_modal']")
+        the_modal_soup = BeautifulSoup(the_modal.get_attribute('innerHTML').encode('utf-8'), 'html.parser')
+
+        # Collect the question string and the link
+        try:
+            answer_title_link = urllib.parse.urljoin(BASE_URL, the_modal_soup.find('a', class_='question_link')['href'])
+        except:
+            print(the_modal.get_attribute('innerHTML'))
+
+        answer_title = the_modal_soup.find('span', class_='question_text').text
+
+        # Collect the answer
+        answer = ''
+        answer_html = the_modal_soup.find_all('p', class_='qtext_para')
+        for a in answer_html:
+            answer += a.text
+
+        # Find the number of upvote for this answer
+        num_upvote = int(the_modal_soup.find('span', class_='count').text)
+
+        close_button = the_modal.find_element_by_class_name('modal_fixed_close')
+        close_button.click()
+        answers_result.append({
+            'title': answer_title,
+            'title_url': answer_title_link,
+            'author_url': url,
+            'answer': answer,
+            'num upvote': num_upvote
+        })
+        with open(ANSWER_OUTPUT_FILE, 'w') as f:
+            json.dump(answers_result, f, indent=4)
+
+
 def main():
     # Initialize webdriver
     driver = webdriver.Chrome()
@@ -75,233 +201,10 @@ def main():
 
     # Loop through all popular writers
     for writer_url in list_of_top_writers:
-        url = urllib.parse.urljoin(BASE_URL, writer_url)
-        driver.get(url)
-        # have to scroll until the end of page
-        current_html = driver.find_element_by_class_name('ContentWrapper')
-        current_html = current_html.get_attribute('innerHTML')
-        stuck_value = 0
-        while(True):
-            prev_html = current_html
-            # scroll to the end of page and set some delay --> to get the questions
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            current_html = driver.find_element_by_class_name('ContentWrapper')
-            current_html = current_html.get_attribute("innerHTML")
-            time.sleep(5)
+        process_user(driver, writer_url)
 
-            if stuck_value > 5:
-                break
-
-            if prev_html == current_html:
-                # if after scrolling nothing changes, that means it is stuck
-                stuck_value += 1
-
-            print('stuck_value: ', stuck_value)
-
+    # finish operation
     driver.close()
-
-    # # find total number of questions
-    # numberOfQuestionsDiv = driver.find_element_by_class_name('TopicQuestionsStatsRow').get_attribute("innerHTML")
-    # numberOfQuestionsSoup = BeautifulSoup(numberOfQuestionsDiv, 'html.parser').strong.text
-    # numberOfQuestions = HTMLNumberToPlain(numberOfQuestionsSoup)
-
-    # # get div with all questions
-    # questionDiv = driver.find_element_by_class_name('layout_2col_main')
-    # questionHTML = questionDiv.get_attribute("innerHTML")
-    # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    # # Allow time to update page
-    # time.sleep(5)
-
-    # # get questions again
-    # questionDiv = driver.find_element_by_class_name('layout_2col_main')
-    # newQuestionHTML = questionDiv.get_attribute("innerHTML")
-
-    # if newQuestionHTML == questionHTML:
-    #     questionsScrapedSoFar = numberOfQuestions
-    # else:
-    #     soup = BeautifulSoup(newQuestionHTML.encode("utf-8"), 'html.parser')
-    #     questionsScrapedSoFarSoup = soup.find_all('a', class_= 'question_link')
-    #     questionsScrapedSoFar=0
-    #     for q in questionsScrapedSoFarSoup:
-    #         questionsScrapedSoFar+=1
-
-    # repeatCount = 0
-    # # Keep checking if there are new questions after scrolling down
-    # while (questionsScrapedSoFar < numberOfQuestionsToScrape):
-    # # while (True):
-    #     questionHTML = newQuestionHTML
-    #     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    #     time.sleep(5)
-    #     questionDiv = driver.find_element_by_class_name('layout_2col_main')
-    #     newQuestionHTML = questionDiv.get_attribute("innerHTML")
-
-    #     if newQuestionHTML != questionHTML:
-    #         # Each time you scroll down, 20 more are added
-    #         questionsScrapedSoFar += 20
-    #         repeatCount = 0
-    #     else:
-    #         # print("STALLING!")
-    #         repeatCount += 1
-
-    #     if questionsScrapedSoFar % 100 == 0:
-    #         print("Questions scraped: ", questionsScrapedSoFar)
-
-    #     # if repeatCount > 40:
-    #     #     print("Quora stalled after scraping " + str(questionsScrapedSoFar) + " questions")
-    #     #     break
-
-
-    # finalQuestions = questionDiv.get_attribute("innerHTML").encode("utf-8")
-
-    # # Get questions as strings
-    # soup = BeautifulSoup(finalQuestions, 'html.parser')
-    # questions = soup.find_all('a', class_= 'question_link')
-    # questionLinks = []
-    # for q in questions:
-    #     questionLinks.append(q['href'])
-
-    # print("NUMBER OF QUESTIONS DISCOVERED: ", len(questionLinks))
-    # # Visit each question page to get stats
-    # questionStats = []
-    # questionsScrapedSoFar = 0
-
-    # print("parsing each question...")
-    # repeatCount = 0
-    # for qLink in questionLinks:
-    #     driver.get('https://www.quora.com' + qLink)
-    #     try:
-    #         oldAnsHtml = driver.find_element_by_class_name('content_page_feed_offset')
-    #     except:
-    #         # either no answer yet, the link is broken, just get the question text
-    #         # Get question text
-    #         try:
-    #             questionsText = driver.find_element_by_class_name('rendered_qtext').text.encode("utf-8")
-    #         except:
-    #             questionsText = None
-    #         try:
-    #             # Get number of followers
-    #             numFollowers = int(driver.find_element_by_class_name('FollowerListModalLink').text.split(" ")[0])
-    #         except:
-    #             numFollowers = 0
-    #         try:
-    #             # Get number of views
-    #             numViews = int(driver.find_element_by_class_name('ViewsRow').text.split(" ")[0].replace(",",""))
-    #         except: 
-    #             numViews = 0
-    #         questionStats.append(
-    #             {
-    #                 "Answer": None,
-    #                 "Answer author": None,
-    #                 "Author link": None,
-    #                 "Answer views": None,
-    #                 "Answer upvotes": None,
-    #                 "Question": questionsText,
-    #                 "Question link": 'https://www.quora.com' + qLink,
-    #                 "Question Followers": numFollowers,
-    #                 "Question views": numViews  
-    #             })
-    #         continue
-
-    #     # loop here first until getting all the possible
-    #     while(True):
-    #         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    #         time.sleep(3)
-    #         newAnsHtml = driver.find_element_by_class_name('content_page_feed_offset')
-    #         if oldAnsHtml == newAnsHtml:
-    #             repeatCount += 1
-    #         else:
-    #             repeatCount = 0
-    #             oldAnsHtml = newAnsHtml
-    #         if repeatCount > 2:
-    #             # Finish scrolling down
-    #             # print("Finished scrolling down: ", qLink)
-    #             break
-
-    #     answers = driver.find_element_by_class_name('AnswerListDiv')
-        
-    #     # Get question text
-    #     questionsText = driver.find_element_by_class_name('rendered_qtext').text.encode("utf-8")  
-    #     # Get number of followers of this question
-    #     numFollowers = int(driver.find_element_by_class_name('FollowerListModalLink').text.split(" ")[0])
-    #     # Get number of views for this question
-    #     numViews = int(driver.find_element_by_class_name('ViewsRow').text.split(" ")[0].replace(",",""))
-    #     # Get number of answers
-    #     numOfAnswers = 0
-
-    #     # Need to get number of answers
-    #     # try:
-    #     #     numberOfAnswersText = driver.find_element_by_class_name('answer_count').text.split(" ")[0].replace(',','').replace('+', '')
-    #     # except:
-    #     #     numberOfAnswersText = 1
-
-    #     if answers is not None:
-    #         newQuestionHTML = answers.get_attribute("innerHTML")
-    #         soup = BeautifulSoup(newQuestionHTML, 'html.parser')
-    #         answer_divs = soup.find_all('div', class_='Answer')
-    #         answer_count = 0
-    #         for answer_div in answer_divs:
-    #             # TODO: Continue
-    #             try:
-    #                 answer_author_object = answer_div.find('a', class_='user')
-    #                 answer_author_link = 'https://www.quora.com' + answer_author_object['href']
-    #                 answer_author = answer_div.find('a', class_='user')
-    #                 answer_author = answer_author_object.get_text()
-    #                 the_answer = '' 
-    #                 answers_p = answer_div.find_all('p', class_='qtext_para')
-    #                 for answer in answers_p:
-    #                     the_answer += answer.get_text()
-    #                 answer_views = answer_div.find('span', class_='meta_num').get_text()
-    #                 if 'k' in answer_views:
-    #                     answer_views = answer_views[0:len(answer_views)-1]
-    #                     answer_views = float(answer_views) * 1000
-    #                 answer_views = int(answer_views)
-    #                 # Get the number of upvotes of each answer
-    #                 answer_upvotes = answer_div.find('span', class_='count').get_text()
-    #                 if 'k' in answer_upvotes:
-    #                     answer_upvotes = answer_upvotes[0:len(answer_upvotes)-1]
-    #                     answer_upvotes = float(answer_upvotes) * 1000
-    #                 answer_upvotes = int(answer_upvotes)
-    #                 questionStats.append({
-    #                     "Answer": the_answer,
-    #                     "Answer author": answer_author,
-    #                     "Author link": answer_author_link,
-    #                     "Answer views": answer_views,
-    #                     "Answer upvotes": answer_upvotes,
-    #                     "Question": str(questionsText), 
-    #                     "Question link": 'https://www.quora.com' + qLink,
-    #                     "Question Followers": numFollowers,
-    #                     "Question views": numViews  
-    #                     })
-    #                 answer_count += 1
-    #             except:
-    #                 continue
-    #         # print("num of answers for: ", qLink," is: ", answer_count)
-    #     # No answer yet
-    #     else:
-    #         questionStats.append(
-    #             {
-    #                 "Answer": None,
-    #                 "Answer author": None,
-    #                 "Author link": None,
-    #                 "Answer views": None,
-    #                 "Answer upvotes": None,
-    #                 "Question": questionsText, 
-    #                 "Question link": 'https://www.quora.com' + qLink,
-    #                 "Question Followers": numFollowers,
-    #                 "Question views": numViews
-    #             })
-
-
-    #     questionsScrapedSoFar += 1
-    #     if questionsScrapedSoFar % 20 == 0:
-    #         print("Processed questions: ", questionsScrapedSoFar)
-    #         print("Saving to file...")
-    #         with open('result2.json', 'w') as file:
-    #             json.dump(questionStats, file, indent=0)
-
-
-    # # Close the window, finished operation
-    # driver.close()
 
 
 if __name__ == '__main__':
